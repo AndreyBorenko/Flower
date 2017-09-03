c6cb6ccb3415ae5df868b6288e1be1ed0cd0a1e5
Andrey Borenko <borenko274@mail.ru>
2017-09-03 12:01:01 +0300

    burger menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,9 @@
 $(function() {
 
-	// Custom JS
+    // Toggle menu
+    $('.icon-burger').click(function (e) {
+        console.log($(this).siblings('.main-menu'));
+        $('.hidden-menu .main-menu').slideToggle();
+    })
 
 });
